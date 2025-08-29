b6db884ffe30a7a86401356044a23e1de8ba0a29
Chayan <chayanbala1992@gmail.com>
2025-08-29 21:46:36 +0600

    Click heart and increase heart count

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-
+const hearts = document.getElementsByClassName("heart");
+for (let heart of hearts) {
+  heart.addEventListener("click", function () {
+    let all_heart = parseInt(
+      document.getElementById("reserve_heart").innerText
+    );
+    all_heart = all_heart + 1;
+    document.getElementById("reserve_heart").innerText = all_heart;
+  });
+}
