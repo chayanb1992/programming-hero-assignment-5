26cb9ff53ca6a0c8bdffba053b77ac1a70902957
Chayan <chayanbala1992@gmail.com>
2025-08-29 23:03:40 +0600

    alert and use coin section

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// heart click function
 const hearts = document.getElementsByClassName("heart");
 for (let heart of hearts) {
   heart.addEventListener("click", function () {
@@ -6,5 +7,28 @@ for (let heart of hearts) {
     );
     all_heart = all_heart + 1;
     document.getElementById("reserve_heart").innerText = all_heart;
+  });
+}
+
+// call button acction
+const call_buttons = document.getElementsByClassName("call");
+console.log(call_buttons);
+for (let call of call_buttons) {
+  call.addEventListener("click", function (event) {
+    const parent = event.target.parentNode.parentNode;
+    const service = parent.getElementsByClassName("service")[0].innerText;
+    let number = parent.getElementsByClassName("number")[0].innerText;
+    // if (number.length > 3) {
+    //   number = number.slice(0, 3);
+    // }
+    // use coin
+    let coin = parseInt(document.getElementById("coin").innerText);
+    if (coin >= 20) {
+      coin = coin - 20;
+      alert("📞" + " " + "Calling" + " " + service + " " + number + "\u2026");
+      document.getElementById("coin").innerText = coin;
+    } else {
+      alert("❌" + " " + "You need at least 20 coins to make a call.");
+    }
   });
 }
